96239fa53097aae5c5650693d13b7c30942198cc
kami1306 <162999455+kami1306@users.noreply.github.com>
2024-04-26 12:10:30 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -14,32 +14,32 @@ def exibir_opcoes():
  print('4. sair')
  
 def finaliza_app():
-   exibir_subtitulo('finalizar aplicativo')
+   exibir_subtitulo('finalizar app')
 
 def voltar_ao_menu_principal():
-   input('\n digite a tecla "enter"para voltar ao menu principal')
+   input('\n digite a tecla "enter"para voltar ao menu principal') 
    main()
 
 def opcao_invalida():
-  print('opcao invalida\n')
+  print('opcao invalida!\n')
   voltar_ao_menu_principal()
 
 def exibir_subtitulo(texto):
     os.system('clear')
-    linha='*'*(len)(texto)
+    linha='*'*(len(texto))
     print(linha)
     print(texto)
     print(linha)
     print()
 
 def cadastrar_novo_restaurantes():
-      exibir_subtitulo('cadastro de novos restaurantes:')
-      nome_do_restaurante=input('digite o nome do novo restaurante')
-      categoria=input(f'digite a categoria do restaurante{nome_do_restaurante}:')
-      dado_do_restaurante={'nome':nome_do_restaurante,'categoria':categoria,'ativo':False}
-      restaurantes.append(dado_do_restaurante)
-      print(f' o restaurante{nome_do_restaurante}foi cadastrado com sucesso!' )
-      voltar_ao_menu_principal()
+ exibir_subtitulo('cadastro de novos restaurantes:') 
+nome_do_restaurante=input('digite o nome do novo restaurante:')
+categoria=input(f'digite a categoria do restaurante{nome_do_restaurante}:')
+dado_do_restaurante={'nome':nome_do_restaurante,'categoria':categoria,'ativo':False}
+restaurantes.append(dado_do_restaurante)
+print(f' o restaurante{nome_do_restaurante}foi cadastrado com sucesso!' )
+voltar_ao_menu_principal()
       
 def listar_restaurante():
         exibir_subtitulo('listando os restaurantes')
@@ -52,16 +52,24 @@ def listar_restaurante():
          print(f'-{nome_do_restaurante.ljust(20)}|{categoria.ljust(20)}|{ativo}')
 
 voltar_ao_menu_principal()
+
 def alternar_estado_restaurante():
    exibir_subtitulo('alternado estado do restaurante')
-   nome_do_restaurante=input('digite o nome do restaurante que deseja alternar o estado:')
-   restaurante_encontrado=False
+   nome_restaurante = input('Digite o nome do restaurante que deseja alternar o estado:')
+   restaurante_encontrado= False
 
    for restaurantes in restaurantes:
-      if nome_do_restaurante==restaurantes['nome']:
-         restaurantes_encontrado=True
-         restaurantes['ativo']=not restaurantes['nome']
-         mensagem=f'o restaurante {nome_do_restaurante}foi ativado com sucesso'if restaurantes{'ativo'else f'o restauranteno
+         if nome_do_restaurante==restaurantes['nome']:
+         restaurantes_encontrado = True
+         restaurantes['ativo']=not restaurantes['ativo']
+         mensagem=f'o restaurante {nome_do_restaurante}foi ativado com sucesso'if restaurantes['ativo']else f'o restaurante{nome_do_restaurante}foi desativado com sucesso!' 
+
+         print(mensagem)
+         
+
+      if not restaurante_encontrado:
+            print('o restaurante não foi encontrado')
+      voltar_ao_menu_principal()
 
 def escolher_opcao():     
    try:     
@@ -72,7 +80,7 @@ def escolher_opcao():
        elif opcao_escolhida==2:
           listar_restaurante()
        elif opcao_escolhida==3:
-          print('ativar restaurantes')
+          alternar_estado_restaurante()
        elif opcao_escolhida==4:
           finaliza_app
        else:
